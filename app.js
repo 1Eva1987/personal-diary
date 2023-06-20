3e21f99402310c4b8a7e1ebe3577c727673ceec3
1Eva1987 <115692772+1Eva1987@users.noreply.github.com>
2023-06-20 19:12:19 +0100

    express-session installed to check if user is loged in and store needed info during session once user is logedin the user can navigate to todo list using the link in personalDiary page if post route for todo created needed info collected using inputs to log current user and item that will be added to users DB document

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
+const session = require("express-session");
 const app = express();
 
 const port = process.env.PORT;
@@ -12,6 +13,14 @@ app.use(bodyParser.urlencoded({ extended: "true" }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
 // connecting to mongoDB
 mongoose
   .connect(process.env.MONGO_CONNECTION)
@@ -19,6 +28,7 @@ mongoose
   .catch((err) => console.log(`Not connected: ${err}`));
 
 const userSchema = new mongoose.Schema({
+  name: String,
   email: {
     type: String,
     required: true,
@@ -27,10 +37,21 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
   },
+  todoList: [
+    {
+      type: String,
+    },
+  ],
 });
 
 const User = new mongoose.model("User", userSchema);
-
+const requireLogin = (req, res, next) => {
+  if (req.session.loggedIn) {
+    next();
+  } else {
+    res.redirect("/login");
+  }
+};
 // GET
 app.get("/", (req, res) => {
   res.render("home");
@@ -44,6 +65,14 @@ app.get("/login", (req, res) => {
   res.render("login");
 });
 
+app.get("/toDo", requireLogin, (req, res) => {
+  res.render("toDo", { usersEmail: req.session.user.email });
+});
+app.post("/toDo", requireLogin, (req, res) => {
+  console.log(req.session.user.email);
+  console.log(req.body);
+});
+
 // POST
 
 // Register a new user and adding to DB, if user already exists redirecting to login page
@@ -52,17 +81,19 @@ app.post("/register", (req, res) => {
     User.find({ email: req.body.email })
       .then((foundUser) => {
         if (foundUser.length > 0) {
-          console.log(foundUser);
+          req.session.user = foundUser;
           res.redirect("/login");
         } else {
           const newUser = new User({
+            name: req.body.name,
             email: req.body.email,
             password: hash,
           });
           newUser
             .save()
             .then(() => {
-              res.render("personalDiary");
+              req.session.loggedIn = true;
+              res.render("personalDiary", { usersName: req.body.name });
             })
             .catch((err) => console.log(err));
         }
@@ -81,10 +112,13 @@ app.post("/login", (req, res) => {
         .compare(password, foundUser.password)
         .then((result) => {
           if (result) {
-            res.render("personalDiary");
+            req.session.user = foundUser;
+            req.session.loggedIn = true;
+            res.render("personalDiary", { usersName: foundUser.name });
           } else {
             console.log("password not matching");
             // ntify user to check login info as password doesnt match
+            // res.render("login", { errorMessage: "Invalid password." });
           }
         })
         .catch((err) => console.log(err));
