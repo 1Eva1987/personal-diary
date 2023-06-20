da3cd96f7b551b38ff19a2f59fdc1c0bb4e5a543
1Eva1987 <115692772+1Eva1987@users.noreply.github.com>
2023-06-20 12:01:48 +0100

    naviogation from home page to login or register pages created, connected to mongoDB primary schema created to save user

--- app.js
+++ app.js
@@ -1,14 +1,42 @@
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const app = express();
+
+const port = process.env.PORT;
 
 app.use(bodyParser.urlencoded({ extended: "true" }));
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 
+// connecting to mongoDB
+mongoose
+  .connect(process.env.MONGO_CONNECTION)
+  .then(() => console.log("Connected to mongoDB"))
+  .catch((err) => console.log(`Not connected: ${err}`));
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
 app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.listen(3000, () => console.log("Server is running on port 3000"));
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
