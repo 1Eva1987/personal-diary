7fed159e09d4e1447287f9b0fd61d73015a32654
1Eva1987 <115692772+1Eva1987@users.noreply.github.com>
2023-06-20 13:41:01 +0100

    handling registration if user already exists he is redirected to log in else new user is created in data base and user is taken to personaldiary page / to save passwords safely bcript installed and used

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const saltRounds = 10;
 const app = express();
 
 const port = process.env.PORT;
@@ -27,6 +29,9 @@ const userSchema = new mongoose.Schema({
   },
 });
 
+const User = new mongoose.model("User", userSchema);
+
+// GET
 app.get("/", (req, res) => {
   res.render("home");
 });
@@ -39,4 +44,34 @@ app.get("/login", (req, res) => {
   res.render("login");
 });
 
+// POST
+// register a new user and adding to DB, if user already exists redirecting to login page
+app.post("/register", (req, res) => {
+  bcrypt.hash(req.body.password, saltRounds).then((hash) => {
+    User.find({ email: req.body.email })
+      .then((foundUser) => {
+        if (foundUser.length > 0) {
+          console.log(foundUser);
+          res.redirect("/login");
+        } else {
+          const newUser = new User({
+            email: req.body.email,
+            password: hash,
+          });
+          newUser
+            .save()
+            .then(() => {
+              res.render("personalDiary");
+            })
+            .catch((err) => console.log(err));
+        }
+      })
+      .catch((err) => console.log(err));
+  });
+});
+
+// LOG OUT button
+app.get("/logout", (req, res) => {
+  res.redirect("/");
+});
 app.listen(port, () => console.log(`Server is running on port ${port}`));
